3a59200b9b5f17a7099f6ae7b485de2a80845f78
denaemill <114959977+denaemill@users.noreply.github.com>
2023-02-19 14:21:14 -0500

    Update server-s.py

--- server-s.py
+++ server-s.py
@@ -6,7 +6,7 @@ import time
 # Creating a class of signals to process some errors
 class Stopper:
     stop = False
-    def _init_(self):
+    def __init__(self):
         signal.signal(signal.SIGQUIT, self.interrupt_handler)
         signal.signal(signal.SIGTERM, self.interrupt_handler)
         signal.signal(signal.SIGINT, self.interrupt_handler)
@@ -14,40 +14,34 @@ class Stopper:
     def interrupt_handler(self, *args):
         self.stop = True
 
-#
-BUFFER_SIZE = 10000
 
-#Getting all of the inputs from the command line
-#If port is too large or too mall error?????????/
+BUFFER_SIZE = 10000
 port = sys.argv[1]
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 sock.bind(("0.0.0.0", int(port)))
-
+sock.settimeout(10) #addr
 sock.listen(1)
 
 msg = b""
+i = 0
 total = 0
-stopping = Stopper()
-#while not stopping.stop: #infinite loop that keeps it connected to client
+
+
 clientSock, addr = sock.accept()
-clientSock.settimeout(10) #addr
 
 clientSock.send(b"accio\r\n")
 
 while True:
-    m = clientSock.recv(BUFFER_SIZE) #####
-    msg += m
+    m = clientSock.recv(1024) #####
+
+    if not m:
+        break
 
     total += len(m)
 
-    if m.find(b"\n") != -1:
-        break
 
-    # Connection is closed by server
-    elif len(m) <= 0:
-        break
+print(total)
 
-print("%d" % total)
 clientSock.close()
